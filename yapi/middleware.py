11dac81b64f3b077a70021df1f4ef4fd8ba35f95
André Tavares <mail@andretavares.com>
2014-05-18 22:43:47 +0100

    Solved bug with configuration fetching (that's what you get by not testing! :P)

--- yapi/middleware.py
+++ yapi/middleware.py
@@ -15,8 +15,8 @@ class XsSharing(object):
     def process_request(self, request):
         if 'HTTP_ACCESS_CONTROL_REQUEST_METHOD' in request.META:
             response = http.HttpResponse()
-            response['Access-Control-Allow-Origin']  = settings['YAPI']['XS_SHARING_ALLOWED_ORIGINS'] 
-            response['Access-Control-Allow-Methods'] = ",".join(settings['YAPI']['XS_SHARING_ALLOWED_METHODS'])
+            response['Access-Control-Allow-Origin']  = settings.YAPI['XS_SHARING_ALLOWED_ORIGINS'] 
+            response['Access-Control-Allow-Methods'] = ",".join(settings.YAPI['XS_SHARING_ALLOWED_METHODS'])
             response['Access-Control-Allow-Headers'] = 'X-Requested-With, Authorization, X-Prototype-Version, Content-type'
             return response
         else:
@@ -27,7 +27,7 @@ class XsSharing(object):
         if response.has_header('Access-Control-Allow-Origin'):
             return response
         else:
-            response['Access-Control-Allow-Origin']  = settings['YAPI']['XS_SHARING_ALLOWED_ORIGINS'] 
-            response['Access-Control-Allow-Methods'] = ",".join(settings['YAPI']['XS_SHARING_ALLOWED_METHODS'])
+            response['Access-Control-Allow-Origin']  = settings.YAPI['XS_SHARING_ALLOWED_ORIGINS'] 
+            response['Access-Control-Allow-Methods'] = ",".join(settings.YAPI['XS_SHARING_ALLOWED_METHODS'])
             response['Access-Control-Allow-Headers'] = 'X-Requested-With, Authorization, X-Prototype-Version, Content-type'
             return response
